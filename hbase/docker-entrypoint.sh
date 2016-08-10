2ab914e593ac53eec7eccee72de4fbd6215fb676
Cosmin Lehene <clehene@adobe.com>
2016-08-10 15:19:13 -0700

    [COSMETIC] - Use bash variable expansion for env var validation

--- hbase/docker-entrypoint.sh
+++ hbase/docker-entrypoint.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-if [ -z "$HDFS_NAMENODE_ENDPOINT" ]; then
-  echo >&2 'You need to specify HDFS_NAMENODE_ENDPOINT environment var.'
-  exit 1
-fi
-
-if [ -z "$HDFS_NAMESPACE_ID" ]; then
-  echo >&2 'You need to specify HDFS_NAMESPACE_ID environment var.'
-  exit 1
-fi
-
-if [ -z "$HBASE_HDFS_ROOTDIR" ]; then
-  echo >&2 'You need to specify HBASE_HDFS_ROOTDIR environment var.'
-  exit 1
-fi
-
-if [ -z "$ZK_QUORUM" ]; then
-  echo >&2 'You need to specify ZK_QUORUM environment var.'
-  exit 1
-fi
-
-if [ -z "$HBASE_ZNODE_PARENT" ]; then
-  echo >&2 'You need to specify HBASE_ZNODE_PARENT environment var.'
-  exit 1
-fi
+: "${HDFS_NAMENODE_ENDPOINT:?env var not set}"
+: "${HDFS_NAMESPACE_ID:?env var not set}"
+: "${HBASE_HDFS_ROOTDIR:?env var not set}"
+: "${ZK_QUORUM:?env var not set}"
+: "${HBASE_ZNODE_PARENT:?env var not set}"
 
 sed -ie "s#HDFS_NAMESPACE_ID#$HDFS_NAMESPACE_ID#g" /hbase/conf/hbase-site.xml
 sed -ie "s#HBASE_HDFS_ROOTDIR#$HBASE_HDFS_ROOTDIR#g" /hbase/conf/hbase-site.xml
