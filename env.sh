369ff86663a3a365faad9bed041e7512c6278ac7
Cosmin Lehene <clehene@gmail.com>
2016-11-11 15:44:01 -0800

    [COSMETIC] - Added env vars descriptions

--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
-export REPO="${REPO:-cellos}"
-export TAG=$(git rev-parse --short HEAD)
+# The default version of HBase we're building the container with
 export VERSION="${VERSION:-1.2.2}"
+# The docker hub repository the Docker container will be taged with and pushed to
+export REPO="${REPO:-cellos}"
+# Tag the container with the short version of the current git SHA
+export TAG=$(git rev-parse --short HEAD)
 export HBASE_DOWNLOAD_URL="${HBASE_DOWNLOAD_URL:-http://mirror.reverse.net/pub/apache/hbase/${VERSION}/hbase-${VERSION}-bin.tar.gz}"
-
