864219e3b7d8ddf1eff1bc5d9d98d0ba8cd821ac
Cosmin Lehene <clehene@gmail.com>
2016-11-11 15:44:19 -0800

    [BUMP] - HBase 1.2.4

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 # The default version of HBase we're building the container with
-export VERSION="${VERSION:-1.2.2}"
+export VERSION="${VERSION:-1.2.4}"
 # The docker hub repository the Docker container will be taged with and pushed to
 export REPO="${REPO:-cellos}"
 # Tag the container with the short version of the current git SHA
