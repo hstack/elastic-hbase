42e469b27060bf5245b1beb75a7050f76faee28d
Adrian Muraru <amuraru@adobe.com>
2017-12-04 12:24:43 +0200

    Support for separated WALDIR
    See https://issues.apache.org/jira/browse/HBASE-17437

--- hbase/docker-entrypoint.sh
+++ hbase/docker-entrypoint.sh
@@ -3,12 +3,17 @@ set -eo pipefail
 
 : "${HDFS_NAMENODE_ENDPOINT:?env var not set}"
 : "${HDFS_NAMESPACE_ID:?env var not set}"
-: "${HBASE_HDFS_ROOTDIR:?env var not set}"
 : "${ZK_QUORUM:?env var not set}"
 : "${HBASE_ZNODE_PARENT:?env var not set}"
+: "${HBASE_ROOTDIR:?env var not set}"
+: "${HBASE_HDFS_WALDIR:?env var not set}"
+
+
+
 
 sed -ie "s#HDFS_NAMESPACE_ID#$HDFS_NAMESPACE_ID#g" /hbase/conf/hbase-site.xml
-sed -ie "s#HBASE_HDFS_ROOTDIR#$HBASE_HDFS_ROOTDIR#g" /hbase/conf/hbase-site.xml
+sed -ie "s#HBASE_ROOTDIR#$HBASE_ROOTDIR#g" /hbase/conf/hbase-site.xml
+sed -ie "s#HBASE_HDFS_WALDIR#$HBASE_HDFS_WALDIR#g" /hbase/conf/hbase-site.xml
 sed -ie "s#ZK_QUORUM#$ZK_QUORUM#g" /hbase/conf/hbase-site.xml
 sed -ie "s#HBASE_ZNODE_PARENT#$HBASE_ZNODE_PARENT#g" /hbase/conf/hbase-site.xml
 
